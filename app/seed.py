d6ff5fb9eca5eb76cbd4ba80a00a742faf51d081
anasofia0 <anasofialaranja@gmail.com>
2023-07-10 10:12:32 -0300

    seed

--- app/seed.py
+++ app/seed.py
@@ -20,3 +20,43 @@ questoes = [
         "resposta_certa": "5"
     },
 ]
+
+users = [
+    {
+        "matricula": "1234",
+        "nome": "pedro",
+        "email": "pedro@unb.br",
+        "professor": True,
+        "senha": "asdfg"
+    },
+    {
+        "matricula": "1",
+        "nome": "ester",
+        "email": "ester@unb.br",
+        "professor": False,
+        "senha": "asdfg"
+    }
+]
+
+exames = [
+    {
+        "nome": "teste1",
+        "professor": 1234,
+        "nota": 10,
+        "data_abertura": datetime.now(),
+        "data_fechamento": datetime.now()
+    }
+]
+
+exames_questoes = [
+    {
+        "exame_id": 1,
+        "questao_id": 1,
+        "nota": 5
+    },
+    {
+        "exame_id": 1,
+        "questao_id": 2,
+        "nota": 5
+    }
+]
