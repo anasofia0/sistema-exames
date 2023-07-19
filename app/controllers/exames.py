7bc074fd8f9451d30ec3dab998ecc4b2d3181027
anasofia0 <anasofialaranja@gmail.com>
2023-07-19 16:38:59 -0300

    exceções para datas erradas

--- app/controllers/exames.py
+++ app/controllers/exames.py
@@ -3,6 +3,7 @@ from flask import Blueprint, render_template, redirect, request
 from ..models import Exame, Questao, QuestaoExame
 from flask_login import current_user, login_required
 from ..forms import CriaExameForm
+from datetime import datetime
 
 bp = Blueprint("criacao_exames", __name__)
 
@@ -33,11 +34,17 @@ def cria_exame():
     form.questoes.choices = [(q.id, q.enunciado) for q in questoes]
 
     if form.validate_on_submit():
+
+        data_abertura = form.data_abertura.data
+        data_fechamento = form.data_fechamento.data
+
+        checa_datas(data_abertura, data_fechamento)
+
         novo_exame = Exame(nome=request.form.get("nome"),
                            professor=matricula,
                            nota=request.form.get("nota"),
-                           data_abertura=form.data_abertura.data,
-                           data_fechamento=form.data_fechamento.data,
+                           data_abertura=data_abertura,
+                           data_fechamento=data_fechamento,
                            duracao = datetime2int(form.duracao.data)
         )
         db.session.add(novo_exame)
@@ -56,3 +63,9 @@ def cria_exame():
 
 def datetime2int(datetime):
     return 3600*datetime.hour + 60*datetime.minute + datetime.second
+
+def checa_datas(data_abertura, data_fechamento):
+    if (data_fechamento-data_abertura).total_seconds() < 0:
+        raise ValueError('Data de abertura depois da data de fechamento')
+    if (data_abertura-datetime.now()).total_seconds() < 60:
+        raise ValueError('Data de abertura com mínimo de um minuto de diferença do momento atual')
